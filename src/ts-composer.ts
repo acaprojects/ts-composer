fa2aa3e4db3bbe37ca676acf99dfd82e688f92fa
Alex Sorafumo <alex@yuion.net>
2019-05-21 10:26:52 +1000

    Add initial project files

---
+++ src/ts-composer.ts
@@ -0,0 +1,9 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+  // import "core-js/fn/array.find"
+  // ...
+
+import "core-js/fn/promise";
+
+export default class DummyClass {
+
+}
