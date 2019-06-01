cd1c94dd6d6b028747347dcd88dc5c0b1d3980ad
Alex Sorafumo <alex@yuion.net>
2019-06-01 13:17:31 +1000

    Fix auth references

--- test/auth/auth.service.spec.ts
+++ test/auth/auth.service.spec.ts
@@ -1,33 +1,26 @@
-
-import { engine, EngineAuthService } from '../../src/auth/auth.service';
-
+import { engine, EngineAuthService } from '../../src/auth/auth.service'
+import { Observable } from 'rxjs'
 
 describe('EngineAuthService', () => {
-    let service: EngineAuthService;
+    let service: EngineAuthService
+    let fake_ajax: Observable<any>
 
     beforeEach(() => {
-        spyOn(engine, 'ajax');
-        localStorage.clear();
+        fake_ajax = new Observable<any>()
+        spyOn(engine, 'ajax').and.returnValue(fake_ajax)
+        localStorage.clear()
         service = new EngineAuthService({
             token_uri: '/auth/token',
             redirect_uri: 'http://localhost:8080/oauth-resp.html',
             scope: 'any'
-        });
-    });
+        })
+    })
 
-    it('should get the authority', () => {
+    it('should get the authority', () => {})
 
-    });
+    it('should return the access token', () => {})
 
-    it('should return the access token', () => {
+    it('should handle auth URL parameters', () => {})
 
-    });
-
-    it('should handle auth URL parameters', () => {
-
-    });
-
-    it('should refresh tokens', () => {
-
-    });
-});
+    it('should refresh tokens', () => {})
+})
