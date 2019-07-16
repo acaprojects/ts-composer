a791438b2590f29d9757acf9420bc4dc23dd8ee1
Alex Sorafumo <alex@yuion.net>
2019-07-16 14:37:39 +1000

    Add auth source class, service and tests

---
+++ src/http/services/auth-sources/auth-source.class.ts
@@ -0,0 +1,13 @@
+import { EngineResource } from '../resources/resource.class'
+import { EngineAuthSourcesService } from './auth-sources.service'
+import { HashMap } from '../../../utilities/types.utilities'
+
+export class EngineAuthSource extends EngineResource<EngineAuthSourcesService> {
+    /** Timestamp the zone was created at, in ms since UTC epoch */
+    public readonly created_at: number
+
+    constructor(protected service: EngineAuthSourcesService, raw_data: HashMap) {
+        super(service, raw_data)
+        this.created_at = raw_data.created_at
+    }
+}
---
+++ test/http/services/auth-sources/auth-sources.service.spec.ts
@@ -0,0 +1,39 @@
+import { of } from 'rxjs'
+
+import { EngineAuthSource } from '../../../../src/http/services/auth-sources/auth-source.class'
+import { EngineAuthSourcesService } from '../../../../src/http/services/auth-sources/auth-sources.service'
+
+describe('EngineDomainsService', () => {
+    let service: EngineAuthSourcesService
+    let http: any
+
+    beforeEach(() => {
+        http = {
+            get: jest.fn(),
+            post: jest.fn(),
+            put: jest.fn(),
+            delete: jest.fn()
+        }
+        service = new EngineAuthSourcesService(http)
+    })
+
+    it('should create instance', () => {
+        expect(service).toBeTruthy()
+        expect(service).toBeInstanceOf(EngineAuthSourcesService)
+    })
+
+    it('allow querying systems index', async () => {
+        http.get.mockReturnValueOnce(of({ results: [{ id: 'test' }], total: 10 }))
+        const result = await service.query()
+        expect(http.get).toBeCalledWith('/control/api/authsources')
+        expect(result).toBeInstanceOf(Array)
+        expect(result[0]).toBeInstanceOf(EngineAuthSource)
+    })
+
+    it('allow querying systems show', async () => {
+        http.get.mockReturnValueOnce(of({ id: 'test' }))
+        const result = await service.show('test')
+        expect(http.get).toBeCalledWith('/control/api/authsources/test')
+        expect(result).toBeInstanceOf(EngineAuthSource)
+    })
+})
